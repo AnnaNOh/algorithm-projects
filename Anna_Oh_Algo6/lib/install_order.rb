b6e7b84483923191a79097aa8828e5d69f655c98
Anna <annaoh1994@gmail.com>
2018-01-09 00:14:37 -0800

    finished topo sort

--- Anna_Oh_Algo6/lib/install_order.rb
+++ Anna_Oh_Algo6/lib/install_order.rb
@@ -8,7 +8,30 @@
 # Import any files you need to
 
 
+require 'topological_sort'
 
 def install_order(arr)
+  vertices = []
 
+  # find max id
+  max_id = 0
+  arr.each do |el|
+    # find max id
+    max_id = el[0] if el[0] > max_id
+    max_id = el[1] if el[1] > max_id
+  end
+  # max_id = arr.flatten.sort[-1]
+
+  # make all vertexes
+  (1..max_id).each do |value|
+    vertices << Vertex.new(value)
+  end
+
+  # make edges
+  edges = []
+  arr.each do |el|
+    edges << Edge.new(vertices[el[0]-1], vertices[el[1]-1])
+  end
+
+  topological_sort(vertices).map { |vertex| vertex.value }.reverse!
 end
