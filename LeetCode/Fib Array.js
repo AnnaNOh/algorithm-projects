098726768b2584f2d3c17f6f8d7c3c4fd64e36d9
Anna <annaoh1994@gmail.com>
2018-01-29 14:12:10 -0800

    median of two sorted arrays

---
+++ LeetCode/Fib Array.js
@@ -0,0 +1,19 @@
+const fibArray = function(n) {
+  const arr = [1,1];
+  if (n === 1){
+    return arr[0];
+  }
+
+  if (n === 2){
+    return arr.slice(0,2);
+  }
+
+  let prevArr = fibArray(n - 1);
+  let newValue = prevArr[prevArr.length - 1] + prevArr[prevArr.length - 2];
+
+
+  return prevArr.concat(newValue);
+
+};
+
+fibArray(12);
---
+++ LeetCode/Median of Two Sorted Arrays.js
@@ -0,0 +1,16 @@
+// Didn't agree with this solution entirely but it works most of the time
+//
+// Example
+// Array A = [1,2,3]
+// Array B = [4,5,6]
+//
+// Index ia starts in the middle of A
+// Index ib starts in the middle of B
+//
+// Then it's three main cases:
+// 1. Median is too little
+// If A[ia] is less than the B[ib -1], move A[ia] over right once
+//
+// 2. Median is right
+//
+// 3. Median is too big
