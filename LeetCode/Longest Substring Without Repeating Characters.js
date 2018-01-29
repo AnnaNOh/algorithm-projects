8cafabe1644cead9be8b7a25a983e86fd676ae1e
Anna <annaoh1994@gmail.com>
2018-01-29 13:22:00 -0800

    Leet Code longest nonrepeating substring

---
+++ LeetCode/Longest Substring Without Repeating Characters.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var lengthOfLongestSubstring = function(s) {
+     let result = 0;
+     let tempCount = 0;
+     let substring = {};
+     let i = 0;
+     let prevIdx = 0;
+     let resultStart = 0;
+
+     while (i < s.length) {
+
+         if (substring.hasOwnProperty(s[i]) && substring[s[i]] >= resultStart) {
+             prevIdx = substring[s[i]];
+             substring[s[i]] = i;
+             tempCount = i - prevIdx;
+             resultStart = prevIdx + 1;
+         } else {
+             substring[s[i]] = i;
+             tempCount += 1;
+             if ( tempCount > result ){
+                 result = tempCount;
+             }
+         }
+
+         i += 1;
+     }
+
+     return result;
+ };
