2993d726d124d132add1270595db392fbc69b283
Anna <annaoh1994@gmail.com>
2018-02-14 15:03:24 -0800

    Leet Code: reverse integer still working

--- LeetCode/06 Zig Zag Conversion.js
+++ LeetCode/06 Zig Zag Conversion.js
@@ -1,4 +1,4 @@
-// Still Working
+// COMPLETED
 
 // Zig Zag Conversion
 //
--- LeetCode/07 Reverse Integer.js
+++ LeetCode/07 Reverse Integer.js
@@ -5,19 +5,21 @@
 // Given a 32-bit signed integer, reverse digits of an integer.
 //
 // Example 1:
-//
 // Input: 123
 // Output:  321
+
 // Example 2:
-//
 // Input: -123
 // Output: -321
+
 // Example 3:
-//
 // Input: 120
 // Output: 21
+
 // Note:
-// Assume we are dealing with an environment which could only hold integers within the 32-bit signed integer range. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+// Assume we are dealing with an environment which could only hold integers
+// within the 32-bit signed integer range. For the purpose of this problem,
+// assume that your function returns 0 when the reversed integer overflows.
 
 // /**
 //  * @param {number} x
@@ -25,5 +27,17 @@
 //  */
 
 var reverse = function(x) {
+  let result = "";
+  let xString = String(x);
 
+
+  let i = 0;
+  while (i < xString.length){
+    result = xString[i] + result;
+    i += 1;
+  }
+
+  result = Number(result);
+
+  return result;
 };
