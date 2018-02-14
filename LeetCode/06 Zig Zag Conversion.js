c883485a0f7c51643165f8f612cade78602d18a0
Anna <annaoh1994@gmail.com>
2018-02-14 11:54:07 -0800

    ZigZag Conversion finished

--- LeetCode/06 Zig Zag Conversion.js
+++ LeetCode/06 Zig Zag Conversion.js
@@ -18,4 +18,38 @@
 // then take 1, 3, 5, 7, (so the odds)
 // then take 2, 6, 10, etc
 
- 
+// the pattern:
+// 1st row     2(r-1)* i    where r = numRows
+// 2nd row     2(r-1) * i +- 1
+// ...
+// rth row     2(r-1) * i +- (r-1)
+
+var convert  = function(s, numRows) {
+  let result = "";
+  let rowIdx = 0;
+  let stringIdx = 0;
+
+  // exception for when numRows === 1
+  if (numRows === 1) {
+    return s;
+  }
+
+  while (rowIdx < numRows) {
+    stringIdx = 0;
+
+    if (s[stringIdx + rowIdx] !== undefined) {
+      result += s[stringIdx + rowIdx];
+    }
+    while (stringIdx < s.length){
+      stringIdx += 2*(numRows-1);
+      if (s[stringIdx - rowIdx] !== undefined && rowIdx !== numRows-1) {
+        result += s[stringIdx - rowIdx];
+      }
+      if (s[stringIdx + rowIdx] !== undefined && rowIdx !== 0) {
+        result += s[stringIdx + rowIdx];
+      }
+    }
+    rowIdx += 1;
+  }
+  return result;
+};
