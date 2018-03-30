bbd413e3d0fd1518a70478f4ca001ea6b42b2483
Anna N. Oh <annaoh1994@gmail.com>
2018-03-29 20:51:43 -0700

    Update 09 Palindrome Number.js

---
+++ LeetCode/09 Palindrome Number.js
@@ -0,0 +1,10 @@
+Determine whether an integer is a palindrome. Do this without extra space.
+
+check the greatest tens -> get its modulo
+check the smallest tens -> get its modulo 
+has to be the same 
+
+all negatives are false
+
+
+
