b927bfe0f55ae364d496d06a668836268ad40db3
Anna N. Oh <annaoh1994@gmail.com>
2018-06-26 13:44:35 -0700

    Create Reverse String.js

---
+++ Algorithm Problems from Google Doc/Reverse String.js
@@ -0,0 +1,12 @@
+
+const reverseStr = (str) => {
+  let result = "";
+  for (let i = str.length - 1; i >= 0; i--){
+    result += str[i];
+  }
+  return result;
+}
+
+let a = "hello";
+reverseStr(a);
+
